e706d73833f7b4ce4039e9f122f69eadc1f7ee88
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2022-11-18 17:51:06 +0100

    updating discover test

--- apps/ledger-live-desktop/tests/models/DiscoverPage.ts
+++ apps/ledger-live-desktop/tests/models/DiscoverPage.ts
@@ -2,11 +2,15 @@ import { Page, Locator } from "@playwright/test";
 
 export class DiscoverPage {
   readonly page: Page;
+  readonly discoverTitle: Locator;
   readonly discoverMenuButton: Locator;
   readonly testAppCatalogItem: Locator;
+  readonly disclaimerTitle: Locator;
   readonly disclaimerText: Locator;
   readonly getAllAccountsButton: Locator;
   readonly requestAccountButton: Locator;
+  readonly selectAssetTitle: Locator;
+  readonly selectAssetSearchBar: Locator;
   readonly selectAccountTitle: Locator;
   readonly selectBtcAsset: Locator;
   readonly selectBtcAccount: Locator;
@@ -17,11 +21,15 @@ export class DiscoverPage {
   constructor(page: Page) {
     this.page = page;
     this.discoverMenuButton = page.locator("data-test-id=drawer-catalog-button");
+    this.discoverTitle = page.locator("data-test-id=discover-title");
     this.testAppCatalogItem = page.locator("#platform-catalog-app-dummy-live-app");
+    this.disclaimerTitle = page.locator("data-test-id=live-app-disclaimer-drawer-title");
     this.disclaimerText = page.locator("text=External Application");
     this.getAllAccountsButton = page.locator("data-test-id=get-all-accounts-button"); // TODO: make this into its own model
     this.requestAccountButton = page.locator("data-test-id=request-single-account-button");
-    this.selectAccountTitle = page.locator("text=Choose a crypto asset)");
+    this.selectAssetTitle = page.locator('data-test-id=select-asset-drawer-title');
+    this.selectAssetSearchBar = page.locator('data-test-id=select-asset-drawer-search-input');
+    this.selectAccountTitle = page.locator('data-test-id=select-account-drawer-title');
     this.selectBtcAsset = page.locator("text=Bitcoin").first();
     this.selectBtcAccount = page.locator("text=Bitcoin 1 (legacy)").first();
     this.disclaimerCheckbox = page.locator("data-test-id=dismiss-disclaimer");
@@ -33,21 +41,25 @@ export class DiscoverPage {
 
   async openTestApp() {
     await this.testAppCatalogItem.click();
+    await this.disclaimerTitle.waitFor({state: "visible"})
   }
 
   async getAccountsList() {
     await this.clickWebviewElement("[data-test-id=get-all-accounts-button]");
   }
-
-  async requestAccount() {
+  
+  async requestAsset() {
     await this.clickWebviewElement("[data-test-id=request-single-account-button]");
+    await this.selectAssetTitle.isVisible();
+    await this.selectAssetSearchBar.isEnabled();
   }
-
+  
   async selectAsset() {
     await this.selectBtcAsset.click();
   }
-
+  
   async selectAccount() {
+    await this.selectAccountTitle.isVisible();
     // TODO: make this dynamic with passed in variable
     await this.selectBtcAccount.click();
   }
@@ -83,5 +95,10 @@ export class DiscoverPage {
     `,
       );
     }, elementName);
+  }
+
+
+  delay(timeout: number) {
+    return new Promise(resolve => setTimeout(() => resolve("delay complete"), timeout));
   }
 }
